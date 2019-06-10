2da0fe1bf948ab381af7468d4e859630aa9b6e75
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-06-10 12:15:06 -0700

    Basic find function working

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 
+const AccountModel = require('./data/accounts-model')
+
 const server = express();
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+   AccountModel.find()
+   .then(item => {
+       res.status(200).json({item, message: "It works"})
+   })
+   .catch(err => {
+       res.status(500).json(err)
+   })
+})
 
 // your code here
 
